2716c0dab726d66ab7ef26feaa33f7788a068b89
chejen <jkopre.qek@gmail.com>
2024-06-17 23:39:58 +0800

    register a new command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const launchPython3Server = vscode.commands.registerCommand(
+		'vscode-sidekick.launchPython3Server',
+		() => {
+			const terminal = vscode.window.createTerminal('Python3 Server Terminal');
+			terminal.show();
+			terminal.sendText('python3 -m http.server', true);
+		}
+	);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-sidekick" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-sidekick.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-sidekick!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(launchPython3Server);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
